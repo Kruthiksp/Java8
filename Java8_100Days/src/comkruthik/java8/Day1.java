8fe97edb4e969fd0ff01073f14d5ddda75578c7a
kruthik_sp <kruthiksp62@gmail.com>
2025-07-17 21:33:48 +0530

    Commit-1 Filtering even numbers from a List

---
+++ Java8_100Days/src/comkruthik/java8/Day1.java
@@ -0,0 +1,21 @@
+package comkruthik.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* 
+ * Filter Even Numbers Using Stream API 
+*/
+
+public class Day1 {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		List<Integer> evenNumbers = numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+
+		System.out.println("Even Numbers: " + evenNumbers);
+	}
+}
