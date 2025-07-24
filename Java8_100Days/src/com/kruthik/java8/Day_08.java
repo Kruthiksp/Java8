eecd171a09a317cefa21a74bd26fe7fc4f65753e
kruthik_sp <kruthiksp62@gmail.com>
2025-07-25 00:27:03 +0530

    code Enhancement and depth understanding of toMap

--- Java8_100Days/src/com/kruthik/java8/Day_08.java
+++ Java8_100Days/src/com/kruthik/java8/Day_08.java
@@ -1,6 +1,7 @@
 package com.kruthik.java8;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,30 +22,40 @@ public class Day_08 {
 		
 		List<Product> products = Arrays.asList(
 			    new Product("Laptop", 50000),
-			    new Product("Laptop", 60000),
 			    new Product("Mouse", 500),
 			    new Product("Keyboard", 1200),
+			    new Product("Pen Drive", 850),
 			    new Product("Monitor", 8000),
+			    new Product("Laptop", 80000),
 			    new Product("Pen Drive", 750)
 			);
 		
 			Map<String, Double> collectedMap = products.stream()
-														.collect(
-																Collectors.toMap(
-																		Product::getName,	//Key Mapper
-																		Product::getPrice,	// Value mapper
-																		(price1, price2) -> (price1 + price2) / 2));	// Merge Function
-																														// Executes only in case of common Keys
+														.sorted(Comparator.comparing(Product::getPrice).reversed())
+														.collect(Collectors.toMap(
+																		Product::getName,
+																		Product::getPrice,
+																		(price1, price2) -> (price1 + price2) / 2
+																	)
+																);
 			
 			System.out.println(collectedMap);
 	}
 }
-/*
-	If product names could be duplicated, you'd need a merge function:
----------------------------------------------------------------------------
-	.collect(Collectors.toMap(
-		    Product::getName,
-		    Product::getPrice,
-		    (price1, price2) -> price1 // or price2 or average
-		));
-*/
+/**
+ * 	If product names could be duplicated, you'd need a merge function
+ * 	And To maintain the Insertion Order we have to use LinkedHashMap
+ * 
+ * 	These are only 4 parameters that can be used inside toMap().
+ * 
+ *---------------------------------------------------------------------------
+ *	.collect(Collectors.toMap(
+ *		    Product::getName,	// Key Mapping.
+ *		    Product::getPrice,	// Value Mapping.
+ *		    (price1, price2) -> (price1 + price2)/2,	// Merge Function, Executes when Keys are duplicated(Instead of throwing IllegalStateException).
+ *			LinkedHashMap::new	// To specify which Implementation Class of Map do we need based on our requirement.
+ *		));
+ *
+ *
+ *	LinkedHashMap::new 		and 	() -> new LinkedHashMap()	are equal
+ */
