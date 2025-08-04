626da4e40068badf115ddd9abeea31998501b7fb
kruthik_sp <kruthiksp62@gmail.com>
2025-08-04 11:21:33 +0530

    Day 19 SOlution

--- Java8_100Days/src/com/kruthik/java8/entities/Item.java
+++ Java8_100Days/src/com/kruthik/java8/entities/Item.java
@@ -4,11 +4,19 @@ public class Item {
 
 	private String name;
 	private double price;
+	private int quantity;
 
 	public Item(String name, double price) {
 		super();
 		this.name = name;
 		this.price = price;
+	}
+
+	public Item(String name, double price, int quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
 	}
 
 	public String getName() {
@@ -19,9 +27,22 @@ public class Item {
 		return price;
 	}
 
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
 	@Override
 	public String toString() {
-		return "Item [name=" + name + ", price=" + price + "]";
+		return "Item [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
 	}
+
+//	@Override
+//	public String toString() {
+//		return "Item [name=" + name + ", price=" + price + "]";
+//	}
 
 }
